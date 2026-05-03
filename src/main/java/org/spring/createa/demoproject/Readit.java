97ef3633142e6c034c6d4514ffff688f4413fd4b
yangmunseok <alex2827@office.kw.ac.kr>
2026-05-03 13:14:56 +0900

    Depoly-1.1.1

--- src/main/java/org/spring/createa/demoproject/Readit.java
+++ src/main/java/org/spring/createa/demoproject/Readit.java
@@ -2,8 +2,10 @@ package org.spring.createa.demoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class Readit {
 
   public static void main(String[] args) {
