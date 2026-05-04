97a18a7ff2c3b87208ba60ab4dd614f18a9af2c4
yangmunseok <alex2827@office.kw.ac.kr>
2026-05-04 14:36:11 +0900

    Depoly-1.1.4

--- src/main/java/org/spring/createa/demoproject/Readit.java
+++ src/main/java/org/spring/createa/demoproject/Readit.java
@@ -2,6 +2,7 @@ package org.spring.createa.demoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
